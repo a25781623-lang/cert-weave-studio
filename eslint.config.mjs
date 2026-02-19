475f21371983073fd90ee532a6a1c38a81871508
a25781623-lang <a25781623@gmail.com>
2026-02-19 17:31:27 +0530

    Fixing Linting Issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,7 +19,9 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-empty-object-type": "warn",
+      "@typescript-eslint/no-require-imports": "warn",
       "@typescript-eslint/no-unused-vars": "off",
     },
   },
